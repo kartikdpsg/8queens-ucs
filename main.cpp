465ded863856d5ad381be4fb840d4f762c8d8873
Kartik Gupta <44387602+kartikdpsg@users.noreply.github.com>
2020-02-05 22:45:44 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+/*ll int power(ll int base,ll int exponent)
+{   
+    if (exponent == 0) return 1;
+    ll int temp = power(base, exponent/2);
+    if (exponent % 2 == 0)
+        return temp * temp; 
+    else
+        return (base * temp * temp);
+}*/
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
